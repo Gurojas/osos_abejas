8a45079ae128e62d116a70b1403dea7b67e2a2cd
Gustafox <gurojas13@alumnos.utalca.cl>
2018-05-29 00:15:14 -0400

    Second commit: ahora parace que funca bien

--- osoabeja.c
+++ osoabeja.c
@@ -3,11 +3,12 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
+#define MAXDELAY 3
 
-sem_t lleno, vacio, mutex;
-int recipiente = 0;
-int in = 0;
-int h;
+sem_t lleno, mutex;
+int porcion = 0;
+int *pote;
+int num_porciones;
 
 void init(sem_t *sem, int valor_inicial) {
    if (sem_init(sem, 0, valor_inicial)) {
@@ -38,19 +39,23 @@ void *proceso_abeja(void *param){
    int *id = (int*)param;
    while(1){
       p(&mutex); // bloqueo para que UNA abeja llene el recipiente
-      printf("abeja %d deposita miel\n",*id);
-      recipiente = recipiente + 1;
-      if (recipiente == h){
-         sleep(rand()%5);
+      //recipiente = recipiente + 1;
+      pote[porcion] = 1;
+      porcion = porcion + 1;
+      if (porcion == num_porciones){
+         printf("abeja %d deposita la porcion %d y avisa al oso que el pote esta lleno\n",*id,porcion);
+         sleep(rand()%MAXDELAY);
          v(&lleno); // despierto al oso
          /*
          Como el proceso de la abeja no va a pasar por el else, entonces no podra entrar 
          otra abeja a llenar el recipiente
          */
       }
-      else{ 
-        sleep(rand()%5);
+      else{
+        printf("abeja %d deposita la porcion %d de miel\n",*id,porcion); 
+        sleep(rand()%MAXDELAY);
         v(&mutex); // doy el paso a otra abeja para que llene el recipiente
+        usleep(200); // duermo un tiempo muy pequeño el proceso para que otro pueda entrar
          
       }
    }
@@ -58,12 +63,16 @@ void *proceso_abeja(void *param){
 }
 
 void *proceso_oso(void *param){
-
    while(1){
       p(&lleno); //el oso comienza a comer miel
-      recipiente = 0;
-      printf("oso consumiendo recipiente con miel\n");
-      sleep(rand()%5); 
+      printf("oso despierta y consume recipiente con miel\n");
+      porcion = 0;
+      for (int i = 0; i < num_porciones; i++){
+         pote[i] = 0;
+      }
+      sleep(rand()%MAXDELAY); // dejo un tiempo para que el oso termine de comer
+      printf("oso comio toda la miel.. ahora se va a dormir\n");
+      sleep(rand()%MAXDELAY); 
       v(&mutex); // el oso termina de comer y desbloquea a las abejas para que empiezen a rellenar el pote
    }
 
@@ -76,31 +85,34 @@ int main(int argc, char *argv[]){
 		exit(EXIT_FAILURE);
 	}
 
-   int n = atoi(argv[1]);
-   h = atoi(argv[2]);
+   int num_abejas = atoi(argv[1]);
+   num_porciones = atoi(argv[2]);
 
-   printf("Numero de abejas: %d\n",n);
-   printf("Capacidad pote: %d\n",h);
+   printf("Numero de abejas: %d\n",num_abejas);
+   printf("Capacidad pote: %d\n",num_porciones);
 
    srand(time(NULL));
 
+   // inicializo el pote 
+   pote = malloc(num_porciones*sizeof(*pote));
+   for (int i = 0; i < num_abejas; i++){
+      pote[i] = 0;
+   }
    
    init(&lleno,0);
-   // semaforo inicializado con el numero de abejas (procesos) que acceden al recurso compartido 
    init(&mutex,1);
 
-   pthread_t p_abeja[n],p_oso;
-   pthread_attr_t attr;
-   pthread_attr_init(&attr);
+   pthread_t p_abeja[num_abejas],p_oso;
 
-   int id_abejas[n];
+   int id_abejas[num_abejas];
 
+   printf("oso dormiendo zZzzZz\n");
    if(pthread_create(&p_oso,NULL,proceso_oso,NULL)){
       printf("Error creando thread oso\n");
       exit(EXIT_FAILURE);
    }
 
-   for(int i = 0; i < n; i++){
+   for(int i = 0; i < num_abejas; i++){
       id_abejas[i] = i;
       if (pthread_create(&p_abeja[i],NULL,proceso_abeja,&id_abejas[i])){
          printf("Error creando thread abeja %d\n",i);
@@ -109,9 +121,6 @@ int main(int argc, char *argv[]){
       
    }
    
-   
-   
-
    pthread_exit(NULL);
 
 }
